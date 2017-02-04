c4c6578ee4753a9c937f4aa282e30e72fd164906
D'Arcey <darceytaylor@gmail.com>
2017-02-04 11:49:52 -0500

    Add project files.

---
+++ OzElectric_EmployeeManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OzElectric_EmployeeManagement.MvcApplication" Language="C#" %>
---
+++ OzElectric_EmployeeManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OzElectric_EmployeeManagement.Startup))]
+namespace OzElectric_EmployeeManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
