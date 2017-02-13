b655e7cab5593533cd3731650d647bbcf32613c5
Fearium <jessebaril.programmer@gmail.com>
2017-02-13 16:48:55 -0500

    Completed JobsModel and Creation View for Jobs. Also modified JobsController

---
+++ OzElectric_EmployeeManagement/Controllers/JobsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OzElectric_EmployeeManagement.Models;
+
+namespace OzElectric_EmployeeManagement.Controllers
+{
+    public class JobsController : Controller
+    {
+        //public string SubTitle { get; set; }
+
+        // NEED TO CONNECT A DB
+        //private ContosoModel db = new ContosoModel();
+
+        // GET: Jobs
+        public async Task<ActionResult> Index()
+        {
+            // this.SubTitle = "Job List";
+            return View(await db.Jobs.ToListAsync());
+        }
+
+        // GET: Jobs/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = await db.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "JobID,Address,City,Province,Foreman,ForemanCell,SiteSuper,PM,Purchaser,GenContractor")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jobs.Add(job);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(job);
+        }
+
+        // GET: Jobs/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = await db.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "JobID,Address,City,Province,Foreman,ForemanCell,SiteSuper,PM,Purchaser,GenContractor")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(job).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(job);
+        }
+
+        // GET: Jobs/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = await db.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Job job = await db.Jobs.FindAsync(id);
+            db.Jobs.Remove(job);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OzElectric_EmployeeManagement/Models/Job.cs
@@ -0,0 +1,46 @@
+﻿namespace OzElectric_EmployeeManagement.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    //[Table("Jobs")]
+    public partial class Job
+    {
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        //public Job()
+        //{
+        //    Enrollments = new HashSet<Enrollment>();
+        //}
+
+        //[Key]
+        public int JobID { get; set; }
+
+        //[Required]
+        //[StringLength(100)]
+        public string LocationName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public string Foreman { get; set; }
+
+        public string ForemanCell { get; set; }
+
+        public string SiteSuper { get; set; }
+
+        public string PM { get; set; }
+
+        public string Purchaser { get; set; }
+
+        public string GenContractor { get; set; }
+
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
