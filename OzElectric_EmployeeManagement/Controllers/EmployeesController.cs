d49d940be9b400e5efe7d0dfc72340bf1826abf4
D'Arcey <darceytaylor@gmail.com>
2017-03-20 16:18:34 -0400

    -Added Sorting to applicable Fields in the Employee Views - Added simple HTML validation to all fields that required it - updated database colum from Province to ProvinceOrState - Added ProvinceOrState drop down menus to the Creat&Edit View for Employee

--- OzElectric_EmployeeManagement/Controllers/EmployeesController.cs
+++ OzElectric_EmployeeManagement/Controllers/EmployeesController.cs
@@ -16,11 +16,62 @@ namespace OzElectric_EmployeeManagement.Controllers
         private ManagementContext db = new ManagementContext();
 
         // GET: Employees
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(string sortOrder)
         {
-            return View(await db.Employees.ToListAsync());
-        }
+            ViewBag.EmployeeNumberSortParm = String.IsNullOrEmpty(sortOrder) ? "EmployeeNumber_desc" : "";
+            ViewBag.FirstNameSortParm = sortOrder == "FirstName" ? "FirstName_desc" : "FirstName";
+            ViewBag.LastNameSortParm = sortOrder == "LastName" ? "LastName_desc" : "LastName";
+            ViewBag.AddressSortParm = sortOrder == "Address" ? "Address_desc" : "Address";
+            ViewBag.CitySortParm = sortOrder == "City" ? "City_desc" : "City";
+            ViewBag.ProvinceOrStateSortParm = sortOrder == "ProvinceOrState" ? "ProvinceOrState_desc" : "ProvinceOrState";
+            var employees = from emp in db.Employees
+                            select emp;
 
+
+            switch (sortOrder)
+            {
+                case "EmployeeNumber_desc":
+                    employees = employees.OrderByDescending(emp => emp.EmployeeNumber);
+                    break;
+                case "FirstName":
+                    employees = employees.OrderBy(emp => emp.FirstName);
+                    break;
+                case "FirstName_desc":
+                    employees = employees.OrderByDescending(emp => emp.FirstName);
+                    break;
+                case "LastName":
+                    employees = employees.OrderBy(emp => emp.LastName);
+                    break;
+                case "LastName_desc":
+                    employees = employees.OrderByDescending(emp => emp.LastName);
+                    break;
+                case "Address":
+                    employees = employees.OrderBy(emp => emp.Address);
+                    break;
+                case "Address_desc":
+                    employees = employees.OrderByDescending(emp => emp.Address);
+                    break;
+                case "City":
+                    employees = employees.OrderBy(emp => emp.City);
+                    break;
+                case "City_desc":
+                    employees = employees.OrderByDescending(emp => emp.City);
+                    break;
+                case "ProvinceOrState":
+                    employees = employees.OrderBy(emp => emp.ProvinceOrState);
+                    break;
+                case "ProvinceOrState_desc":
+                    employees = employees.OrderByDescending(emp => emp.ProvinceOrState);
+                    break;
+                default:
+                    employees = employees.OrderBy(emp => emp.EmployeeNumber);
+                    break;
+
+            }
+            
+            return View(await employees.ToListAsync());
+        }
+             
         // GET: Employees/Details/5
         public async Task<ActionResult> Details(int? id)
         {
@@ -47,7 +98,7 @@ namespace OzElectric_EmployeeManagement.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "EmployeeID,EmployeeNumber,FirstName,LastName,Address,City,Province,HomePhone,HomeCellPhone,WorkPhone,WorkCellPhone,EmergencyContactName,EmergencyContactPhone")] Employee employee)
+        public async Task<ActionResult> Create([Bind(Include = "EmployeeID,EmployeeNumber,FirstName,LastName,Address,City,ProvinceOrState,HomePhone,HomeCellPhone,WorkPhone,WorkCellPhone,EmergencyContactName,EmergencyContactPhone")] Employee employee)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +130,7 @@ namespace OzElectric_EmployeeManagement.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "EmployeeID,EmployeeNumber,FirstName,LastName,Address,City,Province,HomePhone,HomeCellPhone,WorkPhone,WorkCellPhone,EmergencyContactName,EmergencyContactPhone")] Employee employee)
+        public async Task<ActionResult> Edit([Bind(Include = "EmployeeID,EmployeeNumber,FirstName,LastName,Address,City,ProvinceOrState,HomePhone,HomeCellPhone,WorkPhone,WorkCellPhone,EmergencyContactName,EmergencyContactPhone")] Employee employee)
         {
             if (ModelState.IsValid)
             {
