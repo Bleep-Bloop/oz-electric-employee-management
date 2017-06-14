2f8792fefb85a33ec815a5fe0d22c43c9a6a0a6f
Bleep-Bloop <TaisenColcher@Gmail.com>
2017-06-14 19:56:42 -0400

    Returned old routing

--- OzElectric_EmployeeManagement/App_Start/BundleConfig.cs
+++ OzElectric_EmployeeManagement/App_Start/BundleConfig.cs
@@ -73,13 +73,8 @@ namespace OzElectric_EmployeeManagement
                       "~/Scripts/dashboard.js",
                       "~/Scripts/demo.js"));
 
-            //@"%HOME%\site\wwwroot\Views\Account\ForgotPasswordEmailTemplate.html
-            //"@% HOME %\\site\\wwwroot\\Views\\Account\\ForgotPasswordEmailTemplate.html"
-
-            //"@% HOME %\\site\\wwwroot\\Views\\Account\\ForgotPasswordEmailTemplate.html"
-            //"@% HOME %\\site\\wwwroot\\Content//CSS...
-
-            /*  bundles.Add(new StyleBundle("~/bundles/testcss").Include( 
+            
+              bundles.Add(new StyleBundle("~/bundles/testcss").Include( 
                         "~/Content/CSS/BootStrap/bootstrap.min.css",
                         "~/Content/CSS/Custom/AdminLTE.min.css",
                         "~/Content/CSS/Custom/skin-blue.min.css",
@@ -92,30 +87,7 @@ namespace OzElectric_EmployeeManagement
                         "~/Content/Plugins/ionicons2/ionicons2.min.css",
                         "~/Content/Plugins/datatables/dataTables.bootstrap.css",
                         "~/Content/Plugins/fontawesome/font-awesome.min.css"
-                        ));  */
-
-
-            //"@% HOME %\\site\\wwwroot\\Views\\Account\\ForgotPasswordEmailTemplate.html"
-
-            bundles.Add(new StyleBundle("~/bundles/testcss").Include(
-                        @"%HOME%\site\wwwroot\Content\CSS\BootStrap\bootstrap.min.css",
-                        @"%HOME%\site\wwwroot\Content\CSS\Custom\AdminLTE.min.css",
-                        @"%HOME%\site\wwwroot\Content\CSS\Custom\skin-blue.min.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\iCheck\flat\blue.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\morris\morris.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\jvectormap\jquery-jvectormap-1.2.2.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\datepicker\datepicker3.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\daterangepicker\daterangepicker-bs3.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\bootstrap-wysihtml5\bootstrap3-wysihtml5.min.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\ionicons2\ionicons2.min.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\datatables\dataTables.bootstrap.css",
-                        @"%HOME%\site\wwwroot\Content\Plugins\fontawesome\font-awesome.min.css"));
-
-                        
-
-
-
-
+                        ));  
 
 
 
