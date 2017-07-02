d9ac6c68e8b0e6e7b2d25687837786f2c5d22789
D'Arcey <darceytaylor05@gmail.com>
2017-07-02 15:57:46 -0400

    Added Phonebook form to the project includes Create,Delete,Edit, and Details pages includes form validation

---
+++ OzElectric_EmployeeManagement/Models/PhoneBook.cs
@@ -0,0 +1,28 @@
+namespace OzElectric_EmployeeManagement.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("PhoneBook")]
+    public partial class PhoneBook
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ContactId { get; set; }
+
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage = "Not a valid Name.")]
+        public string ContactName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-]([0-9]{3})[-]([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        public string WorkPhone { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-]([0-9]{3})[-]([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        public string WorkCellPhone { get; set; }
+    }
+}
